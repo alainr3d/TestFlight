0cb7923a6cdcf6551165c94d749babf7d2f3c8f3
alainr3d <alaindilig@gmail.com>
2013-12-14 10:51:44 +0800

    update web.js

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE "html">");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Hello World Page</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("Hello World!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
-server.listen(80);
-console.log("Server is listening");
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
