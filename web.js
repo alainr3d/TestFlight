752973c2cdae2d2c1bb445c9ec773b94dbd66484
alainr3d <alaindilig@gmail.com>
2013-12-14 11:10:42 +0800

    next update

--- web.js
+++ web.js
@@ -1,12 +1,9 @@
-var express = require('express');
- 
-var app = express.createServer(express.logger());
- 
-app.get('/', function(request, response) {
-  response.send('hello, world');
-});
- 
-var port = process.env.PORT || 3000;
-console.log("Listening on " + port);
- 
-app.listen(port);
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+
+  console.log("I am working");
+}).listen(8888);
